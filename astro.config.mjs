e10dabb26c7e3776f7de3c07c47bb67d50474326
Christian Romero Leon <christian.romero@grupotawa.com>
2024-10-28 17:16:07 -0500

    Second Intialize

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,9 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
-import auth from 'auth-astro';
+// import auth from 'auth-astro';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), auth()]
+  integrations: [react(), tailwind()]
 });
