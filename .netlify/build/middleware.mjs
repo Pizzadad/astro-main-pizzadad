ecfcbe0953ad671e67ba890548186a084a74b240
Christian Romero Leon <christian.romero@grupotawa.com>
2024-11-24 13:11:21 -0500

    2024-11-24 | Se modifica principal

--- .netlify/build/middleware.mjs
+++
@@ -1,5 +0,0 @@
-import 'es-module-lexer';
-import './chunks/shared_C_dXGrG0.mjs';
-import '@astrojs/internal-helpers/path';
-import 'cookie';
-export { onRequest } from './_astro-internal_middleware.mjs';
