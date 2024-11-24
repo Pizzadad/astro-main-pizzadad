451148686714d5a6b130b80958d9f8b2eb6c2e0e
Christian Romero Leon <christian.romero@grupotawa.com>
2024-11-24 10:57:30 -0500

    2024-11-24 | Se mueve data a public

--- src/Util/get-parfam-json.ts
+++ src/Util/get-parfam-json.ts
@@ -4,7 +4,7 @@ import type { ParfamWithImages } from "@/interfaces/parfam-with-images.interface
 
 
 export const getParfamJson = () => {
-    const filePath = path.resolve("./src/data/perfumes-correct.json");
+    const filePath = path.resolve("./public/data/perfumes-correct.json");
     const fileContent = fs.readFileSync(filePath, "utf-8");
     const parfams: ParfamWithImages[] = JSON.parse(fileContent);
 
